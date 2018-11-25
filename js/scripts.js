1f35e457cb3fe4da30b1036b8124e6ee54400ca2
David L Cervantes <davidlcervantes@Davids-MacBook-Pro.local>
2018-11-25 15:54:57 -0800

    last push all working

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,16 @@ const fah = document.querySelector('h3');
 const oldTemp = prompt('What temperature do you wish to convert?');
 // console.log(oldTemp);
 const temp = parseInt(oldTemp);
-// console.log(temp);
+console.log(temp);
 
 const scale = prompt(
   'To what scale do you wish to convert your temperature?  Please enter "C" or "F":'
 );
-//console.log(scale);
+console.log(scale);
 
 if (scale === 'C') {
-  let newTemp = Math.round((temp - 32) * (5 / 9));
-  // console.log(newTemp);
+  const newTemp = Math.round((temp - 32) * (5 / 9));
+  console.log(newTemp);
   cel.innerHTML = newTemp;
   cel.addEventListener('click', function(event) {
     // console.log(event);
@@ -28,8 +28,8 @@ if (scale === 'C') {
     }
   });
 } else {
-  let newTemp = Math.round(temp * (9 / 5) + 32);
-  // console.log(newTemp);
+  const newTemp = Math.round(temp * (9 / 5) + 32);
+  console.log(newTemp);
   fah.innerHTML = newTemp;
   fah.addEventListener('click', function(event) {
     // console.log(event);
@@ -43,8 +43,3 @@ if (scale === 'C') {
     }
   });
 }
-
-// ///Here is to
-// // Display the temperature in an h3 with minimal styling.
-// From that point on, when the user clicks on the h3, run the function to convert the temperature back and forth between Celcius and Fahrenheit.
-// Display the converted temperature in the h3 each time it is changed.g
